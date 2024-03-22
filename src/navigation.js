29e16883bdb0cce66b50943921bec679842158f5
PKD <pkd@sovietlinux.ml>
2024-03-22 09:03:25 +0100

    fixes

--- src/navigation.js
+++ src/navigation.js
@@ -44,9 +44,8 @@ export const footerData = {
   ],
   socialLinks: [
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://instagram.com/paul_k.d' }, /*TODO: Mkae proper instagram*/ 
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/unsuspicious-industries' },
   ],
   footNote: `
     <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
