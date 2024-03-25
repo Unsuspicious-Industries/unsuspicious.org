2c8ce892204b0433e2972ca84ff81ea2fc082c66
PKD <pkd@sovietlinux.ml>
2024-03-25 22:17:20 +0100

    fixing hero

--- src/navigation.js
+++ src/navigation.js
@@ -16,8 +16,8 @@ export const headerData = {
       href: getPermalink('/about'),
     },
     {
-      text: 'support',
-      href: getPermalink('/support'),
+      text: 'contact',
+      href: getPermalink('/contact'),
     },
     {
       text: 'blog',
@@ -41,14 +41,15 @@ export const footerData = {
   ],
   secondaryLinks: [
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
+    { text: "Contact", href: getPermalink('/contact')}
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/unsuspiciousind' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://instagram.com/paul_k.d' }, /*TODO: Mkae proper instagram*/ 
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/unsuspicious-industries' },
   ],
   footNote: `
-    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(logo.png)]"></span>
-    <a class="text-blue-600 underline dark:text-muted" href="https://unsuspicious.org/pirvacy"> Unsuspicious</a> · All rights reserved.
+    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(~/assets/logo/logo.png)]"></span>
+    <a class="text-blue-600 underline dark:text-muted" href="/"> Unsuspicious</a> · All rights reserved.
   `,
 };
